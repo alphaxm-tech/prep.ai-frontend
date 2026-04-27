62709919f51ed4afe3f7bad771940e62c20556ac
sanket_35 <81851656+sanket531998@users.noreply.github.com>
2026-04-28 01:23:28 +0530

    Update BASE_API_URL to new server address

--- utils/api/endpoints.ts
+++ utils/api/endpoints.ts
@@ -1,5 +1,6 @@
 // export const BASE_API_URL = `http://localhost:8080/api/v1`;
-export const BASE_API_URL = `https://prep-ai-backend-zcbs.onrender.com/api/v1`;
+// export const BASE_API_URL = `https://prep-ai-backend-zcbs.onrender.com/api/v1`;
+export const BASE_API_URL = `http://43.205.142.104:8080/api/v1`;
 
 export const RESUME = `resume`;
 export const AIINTERVIEW = `ai-interview`;
